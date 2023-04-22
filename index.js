768fbcadaf22f2e95f8266175b97f9bcdfb3f790
19ranjan <108362948+19ranjan@users.noreply.github.com>
2023-04-23 01:05:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-const express=require('express')
-const mongoose=require("mongoose")
-const dotenv=require('dotenv').config()
-const cors=require('cors')
-const authController = require('./controllers/authController')
-const propertyController = require('./controllers/propertyController')
-const uploadController=require('./controllers/uploadController')
-const app=express()
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 
-//mongodb connect
-mongoose.set('strictQuery',false);
-mongoose.connect(process.env.MONGO_URL, ()=>console.log("mongo succefuoll"));
-app.use('/files', express.static('public/images'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
-// http://localhost:5000/images/safsfkaefk
-
-//route and middlewares
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use("/auth", authController)
-app.use("/property", propertyController)
-app.use("/upload", uploadController)
-
-//starting server
-app.listen(process.env.PORT,()=>console.log('server has been started successfully '))
