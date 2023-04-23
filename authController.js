d23b83e47e1fe4c492f910ddef37806ba3a521a8
19ranjan <108362948+19ranjan@users.noreply.github.com>
2023-04-23 18:55:57 +0530

    Add files via upload

---
+++ authController.js
@@ -0,0 +1,51 @@
+const authController= require('express').Router()
+const User=require('../models/User')
+const bcrypt= require("bcrypt")
+const jwt=require('jsonwebtoken')
+//const email={User.req.body.email};
+//register
+authController.post('/register', async(req,res)=>{
+    try{
+        //await User('email').findOne(); // Works!
+        const isExisting = await User.findOne({email:req.body.email}).lean();
+        if(isExisting){
+            throw new Error("alresay registered!")
+        }
+
+        const hashedPassword=await bcrypt.hash(req.body.password, 10)
+
+        const newUser= await User.create({...req.body, password:hashedPassword})
+
+        const {password, ...others}=newUser._doc
+        const token=jwt.sign({id:newUser._id}, process.env.JWT_SECRET, {expiresIn:'9d'})
+
+        return res.status(201).json({others,token})
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+//login
+
+authController.post('/login', async(req,res)=>{
+    try{
+        const user=await User.findOne({email:req.body.email})
+        if(!user){
+            throw new Error("wrong cred!")
+        }
+        const comparePass=await bcrypt.compare(req.body.password, user.password)
+        if(!comparePass){
+            throw new Error("wrong cred!")
+        }
+
+        const token= jwt.sign({id: user,_id}, process.env.JWT_SECRET,{expiresIn:'4d'})
+        const {password,...others}=user._doc
+
+        return res.status(200).json({others, token})
+    }
+    catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+module.exports=authController
---
+++ propertyController.js
@@ -0,0 +1,132 @@
+const Property= require('../models/Property')
+const propertyController=require('express').Router()
+const verifyToken =require("../middlewares/verifyToken")
+
+//get all
+propertyController.get('/getAll',async(req,res)=>{
+    try{
+        const properties= await Property.find({})
+
+        return res.status(200).json(properties)
+    }
+    catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+//get featured 
+propertyController.get('/find/featured', async(req,res)=>{
+    try{
+        const featuredProperties =await Property.find({featured:true}).populate('currentOwner', '-password')
+        return res.status(200).json(featuredProperties)
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+//get all from a specific tyoe
+propertyController.get('/find', async(req,res)=>{
+    const type =req.query
+    //{type: 'beach}
+    try{
+        if(type){
+            const properties= await Property.find(type).populate('currentOwner', '-password')
+            return res.status(200).json(properties)
+        }
+        else{
+            return res.status(500).json({msg:"NO such type"})
+        }
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+
+//get counts of types ->ex:{beach:2,village:5, mountains:12}
+propertyController.get('/find/types', async(req,res)=>{
+    try{
+        const beachType= await Property.countDocuments({type:'agra'})
+        const mountainType= await Property.countDocuments({type:'kota'})
+        const villageType= await Property.countDocuments({type:'noida'})
+
+        return res.status(200).json({
+            agra:beachType,
+            kota:mountainType,
+            noida:villageType
+        })
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+
+
+//get individual pro[erty
+propertyController.get("/find/:id",async(req,res)=>{
+    try{
+        const property=await Property.findById(req.params.id).populate("currentOwner", '-password')
+        if(!property){
+            throw new Error("No such property with this id")
+        }else {
+            return res.status(200).json(property)
+        }
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+
+//create an property
+propertyController.post('/',verifyToken, async(req,res)=>{
+    try{console.log("prop");
+        const newProperty =await Property.create({...req.body, currentOwner: req.user.id})
+
+        return res.status(201).json(newProperty)
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+
+//update property
+propertyController.put('/:id',verifyToken, async(req,res)=>{
+    try{
+      const property= await Property.findById(req.params.id)
+      if(property.currentOwner.toString() !==req.user.id.toString()){
+        throw new Error("ypu are not allowed to update other people properties")
+      }
+      else{
+        const updatedProperty = await Property.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},
+            {new:true}
+        )
+        return res.status(200).json(updatedProperty)
+      }
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+
+
+
+//delete property
+propertyController.delete('/:id',verifyToken, async(req,res)=>{
+    try{
+      const property= await Property.findById(req.params.id)
+      if(property.currentOwner.toString()!==req.user.id.toString()){
+        throw new Error("ypu are not allowed to delete other people properties")
+      }
+      else{
+        await property.delete()
+
+        
+        return res.status(200).json({msg:'successfully deleted property'})
+      }
+
+    }catch(error){
+        return res.status(500).json(error.message)
+    }
+})
+module.exports=propertyController
